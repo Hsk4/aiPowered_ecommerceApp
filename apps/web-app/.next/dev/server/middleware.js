d87116ffdd9c9aaf31ccbc6dde7818e32ea1ca4b
Hsk4 <hsk42116@gmail.com>
2026-05-29 04:31:40 +0500

    chore: stop tracking next build output

--- apps/web-app/.next/dev/server/middleware.js
+++
@@ -1,9 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/node_modules_next_headers_0dhts31.js")
-R.c("server/chunks/node_modules_next_0jy4v7s._.js")
-R.c("server/chunks/node_modules_@clerk_shared_dist_runtime_0wh~~d8._.js")
-R.c("server/chunks/node_modules_@clerk_backend_dist_02li.0p._.js")
-R.c("server/chunks/node_modules_@clerk_nextjs_dist_esm_0oqjc11._.js")
-R.c("server/chunks/[root-of-the-server]__0svucvo._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/apps/web-app/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/apps/web-app/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)").exports
--- apps/web-app/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nUeOuTwqJPmaZJgueNZ89Tk4y9Y6Y1zAfN+Kk/tdYGc=\"\n}"
